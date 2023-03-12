0e9a82771d4b27285adf7053770097e5811b1809
cyberpelican <62749885+dhay3@users.noreply.github.com>
2023-03-12 14:41:50 +0800

    Update wima.py

--- wima.py
+++ wima.py
@@ -1,34 +1,56 @@
 import json
 import aiohttp
+import asyncio
+import os
+import random
+import threading
+from lxml import etree
 
-__TIMES__ = 10
+# from multiprocessing import cpu_count
+
+__TIMES__ = 15
+result = set()
+lock = threading.Lock()
+
+
+def main():
+    def _():
+        if 'nt' == os.name:
+            asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
+            asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+            asyncio.run(wima())
+        else:
+            asyncio.run(wima())
+
+    from concurrent.futures import ThreadPoolExecutor
+    with ThreadPoolExecutor(max_workers=__TIMES__) as executor:
+        for idx in range(0, __TIMES__):
+            executor.submit(_)
 
 
 async def wima():
-    from lxml import etree
     conn = aiohttp.TCPConnector(ssl=False, ttl_dns_cache=30)
     timeout = aiohttp.ClientTimeout(total=60)
-    async with aiohttp.ClientSession(timeout=timeout, connector=conn) as s:
-        for idx in range(0, __TIMES__):
-            r = await do(s, 'https://ipinfo.io/')
-            x = etree.HTML(r).xpath('//script[@id="__NEXT_DATA__" and @type="application/json"]')
-            if x:
-                u_ip = json.loads(x[0].text)['props']['pageProps']['userIp']
-                r = await do(s, 'https://ipinfo.io/widget/demo/%s' % u_ip)
-                try:
-                    d = json.loads(r)['data']
-                    ip = d['ip']
-                    network = d['abuse']['network'] if 'abuse' in d else None
-                    city = d['city']
-                    region = d['region']
-                    country = d['country']
-                    org = d['org']
-                    print('ip: %s network: %s city: %s region: %s country: %s org: %s' % (
-                        ip, network, city, region, country, org))
-                except Exception as e:
-                    exit(e)
-            else:
-                pass
+    async with aiohttp.ClientSession(timeout=timeout, connector=conn) as session:
+        r = await do(session, 'https://ipinfo.io/')
+        x_path = '//script[@id="__NEXT_DATA__" and @type="application/json"]'
+        x = etree.HTML(r).xpath(x_path)
+        if x:
+            u_ip = json.loads(x[0].text)['props']['pageProps']['userIp']
+            lock.acquire()
+            if u_ip not in result:
+                r = await do(session, 'https://ipinfo.io/widget/demo/%s' % u_ip)
+                d = json.loads(r)['data']
+                ip = d['ip']
+                network = d['abuse']['network'] if 'abuse' in d else '_'
+                city = d['city']
+                region = d['region']
+                country = d['country']
+                org = d['org']
+                print('ip: %s network: %s city: %s region: %s country: %s org: %s' %
+                      (ip, network, city, region, country, org))
+                result.add(u_ip)
+            lock.release()
 
 
 async def do(session, url):
@@ -43,7 +65,6 @@ async def do(session, url):
 
 
 def random_ua():
-    import random
     return random.choice([
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 '
         'Safari/537.36',
@@ -54,12 +75,4 @@ def random_ua():
 
 
 if __name__ == '__main__':
-    import asyncio
-    import os
-
-    if 'nt' == os.name:
-        asyncio.set_event_loop_policy(asyncio.WindowsProactorEventLoopPolicy())
-        asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-        asyncio.run(wima())
-    else:
-        asyncio.run(wima())
+    main()
